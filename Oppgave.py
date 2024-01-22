c2cf4bb288c78fda60ef6f6c10257ef45d0359df
aasmund-mjos <asmund.mjos@ntnu.no>
2024-01-22 21:06:11 +0100

    Added illustrationPolymer and syntax change in 1b)

--- Oppgave.py
+++ Oppgave.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 """
@@ -6,7 +7,24 @@ import numpy as np
 """
 def createPolymer(N):
     polymer = np.zeros((N,2)) 
-    polymer[:,1] = N//2         # setter y-koordinat
-    for i in range(N):
-        polymer[i,0] = i        # setter x-koordinat
+    polymer[:,1] = N//2                                     # setter y-koordinat
+    polymer[:,0] = np.array([i for i in range(N)])          # setter x-koordinat
     return polymer
+"""
+1 d) Her lager vi illustrasjon av polymeret vårt, det er representert med farge, der sterkere farge er monomer med høyere nummer
+"""
+def illustrationPolymer(polymer):
+    N=len(polymer)                 
+    grid=np.zeros((N+1,N+1))        # Lager (N+1)*(N+1) grid
+    grid-=int(N/2)                         # Setter bakgrunnsverdien til å være -N for å få synlighet blant lave N
+    for monomerNumber in range(N):
+        x=int(polymer[monomerNumber,0])  
+        y=int(polymer[monomerNumber,1])
+        grid[y,x]=monomerNumber+1
+    plt.pcolormesh(grid)
+    plt.show()
+
+N=1000
+polymer=createPolymer(N)
+illustrationPolymer(polymer)
+    

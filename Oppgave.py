924c19628a9f2e44fadff4b32220a482bf17bef3
OliverRuden <oliverruden03@gmail.com>
2024-01-22 20:36:26 +0100

    Gjort oppgave 1b

--- Oppgave.py
+++ Oppgave.py
@@ -1,2 +1,12 @@
-def helloworld():
-    print("Hello World")
+import numpy as np
+
+
+"""
+1 b) Her lager vi polymeret vårt, det er representert i et N*2 diagram, og vi har valgt å holde fast opprunnet midten hvis relevant
+"""
+def createPolymer(N):
+    array = np.zeros((N,2))
+    array[:,1] = N//2
+    for i in range(N):
+        array[i,0] = i
+    return array

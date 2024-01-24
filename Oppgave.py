ad1d9febd893189e5a34b111a7d740cb0baa08ad
OliverRuden <oliverruden03@gmail.com>
2024-01-24 11:47:47 +0100

    Rotasjon er løst?

--- Oppgave.py
+++ Oppgave.py
@@ -36,15 +36,47 @@ def validPolymer(polymer, N):
     coordinateSet.add((polymer[0, 0], polymer[0, 1]))
 
     for index in range(1, N):
-        if (polymer[index, 0], polymer[index, 1]) in coordinateSet:         # sjekker om andre polymerer har samme koordinat
+        if (polymer[index, 0], polymer[index, 1]) in coordinateSet:         # sjekker om andre monomer har samme koordinat
             return False
         else: 
             coordinateSet.add((polymer[index, 0], polymer[index, 1]))
 
-        xDiff = np.abs(polymer[index, 0] - polymer[index - 1, 0])           # avstand i x til nabo-polymer
-        yDiff = np.abs(polymer[index, 1] - polymer[index - 1, 1])           # avstand i y til nabo-polymer
+        xDiff = np.abs(polymer[index, 0] - polymer[index - 1, 0])           # avstand i x til nabo-monomer
+        yDiff = np.abs(polymer[index, 1] - polymer[index - 1, 1])           # avstand i y til nabo-monomer
 
         if xDiff + yDiff != 1:                                              
             return False
         
     return True
+
+"""
+1 f) Implementerer rotasjon ov polymeret, som nevnt tidligere holdes opprundet midten fast.
+"""
+def rotationGoBrrrr(polymer, monomer, positivRetning):
+    middleMonomer = len(polymer)//2 #Finner midterste rundet opp, for å låse den...
+    if middleMonomer > monomer:
+        polymer[:monomer,0] = (2*positivRetning-1)*polymer[:monomer,1]
+        polymer[:monomer,1] = (1-2*positivRetning)*polymer[:monomer,0]
+        """
+        Positiv retning:
+        x = y
+        y = -x
+        Negativ retning:
+        x = -y
+        y = x
+
+        Bruker også at True kan brukes som 1 og False som 0
+        """
+        return polymer
+
+    polymer[monomer+1:,0] = (1-2*positivRetning)*polymer[monomer+1:,1]
+    polymer[monomer+1:,1] = (2*positivRetning-1)*polymer[monomer+1:,0]
+    """
+        Positiv retning:
+        y = x
+        x = -y
+        Negativ retning:
+        y = -x
+        x = y
+    """
+    return polymer

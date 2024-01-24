c2cb4617dfe31979a39a4449104cf3052ef05b02
Astrid Mysterud <astrimys@stud.ntnu.no>
2024-01-24 10:57:23 +0100

    sjekker intakt polymer

--- Oppgave.py
+++ Oppgave.py
@@ -6,20 +6,45 @@ import matplotlib.pyplot as plt
 1 b) Her lager vi polymeret vårt, det er representert i et N*2 diagram, og vi har valgt å holde fast opprunnet midten hvis relevant
 """
 def createPolymer(N):
-    polymer = np.zeros((N,2)) 
-    polymer[:,1] = N//2                                     # setter y-koordinat
-    polymer[:,0] = np.array([i for i in range(N)])          # setter x-koordinat
+    polymer = np.zeros((N, 2)) 
+    polymer[:, 1] = N // 2                                     # setter y-koordinat
+    polymer[:, 0] = np.array([i for i in range(N)])          # setter x-koordinat
     return polymer
 """
 1 d) Her lager vi illustrasjon av polymeret vårt, det er representert med farge, der sterkere farge er monomer med høyere nummer
 """
 def illustrationPolymer(polymer):
-    N=len(polymer)                 
-    grid=np.zeros((N+1,N+1))        # Lager (N+1)*(N+1) grid
-    grid-=int(N/2)                         # Setter bakgrunnsverdien til å være -N for å få synlighet blant lave N
+    N = len(polymer)                 
+    grid = np.zeros((N+1,N+1))        # Lager (N+1)*(N+1) grid
+    grid -= int(N/2)                         # Setter bakgrunnsverdien til å være -N for å få synlighet blant lave N
     for monomerNumber in range(N):
-        x=int(polymer[monomerNumber,0])  
-        y=int(polymer[monomerNumber,1])
-        grid[y,x]=monomerNumber+1
+        x = int(polymer[monomerNumber, 0])  
+        y = int(polymer[monomerNumber, 1])
+        grid[y,x] = monomerNumber + 1
     plt.pcolormesh(grid)
     plt.show()
+
+"""
+1 e) Sjekker om intakt polymer
+"""
+
+def validPolymer(polymer, N):
+    if len(polymer) != N:
+        return False
+    
+    coordinateSet = set()
+    coordinateSet.add((polymer[0, 0], polymer[0, 1]))
+
+    for index in range(1, N):
+        if (polymer[index, 0], polymer[index, 1]) in coordinateSet:         # sjekker om andre polymerer har samme koordinat
+            return False
+        else: 
+            coordinateSet.add((polymer[index, 0], polymer[index, 1]))
+
+        xDiff = np.abs(polymer[index, 0] - polymer[index - 1, 0])           # avstand i x til nabo-polymer
+        yDiff = np.abs(polymer[index, 1] - polymer[index - 1, 1])           # avstand i y til nabo-polymer
+
+        if xDiff + yDiff != 1:                                              
+            return False
+        
+    return True

b2ed453bd9bdc032d483b0da48f37dbda1ad4ffd
OliverRuden <oliverruden03@gmail.com>
2024-01-30 11:54:41 +0100

    Even more speed (also with energy calculations)

--- Oppgave.py
+++ Oppgave.py
@@ -6,9 +6,8 @@ import timeit
 class Middle:
     def __init__(self, x, y, N):
         self.position = np.array([x,y])
-        self.array = np.array([np.array((0,2)) for i in range(N)])
-        self.array[0][0] = -1
-        self.array[N-1][1] = -1
+        self.beforeMiddle = np.array([0 for i in range(N//2)])
+        self.afterMiddle = np.array([2 for i in range((N-1)//2)])
         self.map = {0:np.array([0,-1]), 1:np.array([1,0]), 2:np.array([0,1]),3:np.array([-1,0])}
 
 """
@@ -21,23 +20,23 @@ def createPolymer(N):
 1 d) Her lager vi illustrasjon av polymeret vårt, det er representert med farge, der sterkere farge er monomer med høyere nummer
 """
 def illustrationPolymer(polymer):
-    N = len(polymer.array)                 
+    N = len(polymer.beforeMiddle) + len(polymer.afterMiddle) + 1        
     grid = np.zeros((N+1,N+1))        # Lager (N+1)*(N+1) grid
     grid -= int(N/2)                         # Setter bakgrunnsverdien til å være -N for å få synlighet blant lave N
     index = N//2
     position = np.copy(polymer.position)
     direction = 0
-    for firstMonomers in range(index,0,-1):
-        direction = (direction + polymer.array[firstMonomers,0])%4
+    for firstMonomers in range(index-1,-1,-1):
+        direction = (direction + polymer.beforeMiddle[firstMonomers])%4
         position += polymer.map[direction]
-        grid[position[0],position[1]] = firstMonomers
+        grid[position[0],position[1]] = firstMonomers + 1
     direction = 2
     position = np.copy(polymer.position)
-    grid[position[0],position[1]] = N//2 + 1
-    for secondMonomers in range(index, N-1):
-        direction = (direction + polymer.array[secondMonomers,1]-2)%4
+    grid[position[0],position[1]] = index + 1
+    for secondMonomers in range(0, len(polymer.afterMiddle)):
+        direction = (direction + polymer.afterMiddle[secondMonomers]-2)%4
         position += polymer.map[direction]
-        grid[position[0],position[1]] = secondMonomers+2
+        grid[position[0],position[1]] = secondMonomers+ index + 2
     plt.pcolormesh(grid)
     plt.show()
 
@@ -46,20 +45,17 @@ def illustrationPolymer(polymer):
 """
 
 def validPolymer(polymer, N):
-    if len(polymer.array) != N:
+    if len(polymer.beforeMiddle) + len(polymer.afterMiddle) + 1 != N:
         return False
-    
     coordinateSet = set()
     coordinateSet.add((polymer.position[0], polymer.position[1]))
     index = N//2
     position = np.copy(polymer.position)
     direction = 0
-    for firstMonomers in range(index,0,-1):
-        if polymer.array[firstMonomers][0] not in polymer.map:
+    for firstMonomers in range(index-1,-1,-1):
+        if polymer.beforeMiddle[firstMonomers] not in polymer.map:
             return False
-        elif polymer.array[firstMonomers][1] not in polymer.map:
-            return False
-        direction = (direction + polymer.array[firstMonomers,0])%4
+        direction = (direction + polymer.beforeMiddle[firstMonomers])%4
         position += polymer.map[direction]
         if (position[0],position[1]) in coordinateSet:
             return False
@@ -67,12 +63,10 @@ def validPolymer(polymer, N):
             coordinateSet.add((position[0],position[1]))
     direction = 2
     position = np.copy(polymer.position)
-    for secondMonomers in range(index,N-1):
-        if polymer.array[secondMonomers][0] not in polymer.map:
+    for secondMonomers in range(0,len(polymer.afterMiddle)):
+        if polymer.afterMiddle[secondMonomers] not in polymer.map:
             return False
-        elif polymer.array[secondMonomers][1] not in polymer.map:
-            return False
-        direction = (direction + polymer.array[secondMonomers,1]-2)%4
+        direction = (direction + polymer.afterMiddle[secondMonomers]-2)%4
         position += polymer.map[direction]
         if (position[0],position[1]) in coordinateSet:
             return False
@@ -80,15 +74,14 @@ def validPolymer(polymer, N):
             coordinateSet.add((position[0],position[1]))
     return True
 
-
 """
 1 f) Implementerer rotasjon ov polymeret, som nevnt tidligere holdes opprundet midten fast.
 """
 def rotationGoBrrrr(polymer, monomer, positivRetning):
-    monomer -= 1
-    middleMonomer = len(polymer.array)//2 #Finner midterste rundet opp, for å låse den...
+    monomer -= 2
+    middleMonomer = len(polymer.beforeMiddle)-1 #Finner midterste rundet opp, for å låse den...
     if middleMonomer > monomer:
-        polymer.array[monomer,0] = (polymer.array[monomer,0] + 2*positivRetning-1) % 4
+        polymer.beforeMiddle[monomer] = (polymer.beforeMiddle[monomer] + 2*positivRetning-1) % 4
         """
         Positiv retning:
         delta x = delta y
@@ -100,7 +93,7 @@ def rotationGoBrrrr(polymer, monomer, positivRetning):
         Bruker også at True kan brukes som 1 og False som 0
         """
         return polymer
-    polymer.array[monomer,1] = (polymer.array[monomer,1] - 2*positivRetning+1) % 4
+    polymer.afterMiddle[monomer-middleMonomer] = (polymer.afterMiddle[monomer-middleMonomer] - 2*positivRetning+1) % 4
     """
         Positiv retning:
         delta y = delta x
@@ -162,3 +155,61 @@ def plotValidPercentage(min = 4, max = 500, Ns = 1000):
 # illustrationPolymer(pol)
 
 # plotValidPercentage(10, 500)
+
+"""
+Regne ut energien til polymer
+"""
+def calculateEnergy(polymer, V):
+    total = 0
+    neighbourDictionary = {}
+    coordinates = np.copy(polymer.position)
+    for neighbour in polymer.map.values():
+        neighbourDictionary[tuple(coordinates + neighbour)] = [len(polymer.beforeMiddle)]
+    direction = 0
+    coordinates = np.copy(polymer.position)
+    for firstMonomers in range(len(polymer.beforeMiddle)-1,-1,-1):
+        direction = (direction + polymer.beforeMiddle[firstMonomers])%4
+        coordinates += polymer.map[direction]
+        cordTuple = tuple(coordinates)
+        if cordTuple in neighbourDictionary:
+            for nodes in neighbourDictionary[cordTuple]:
+                total += V[nodes, firstMonomers]
+        for neighbour in polymer.map.values():
+            if tuple(coordinates + neighbour) in neighbourDictionary:
+                neighbourDictionary[tuple(coordinates + neighbour)].append(firstMonomers)
+            else:
+                neighbourDictionary[tuple(coordinates + neighbour)] = [firstMonomers]
+    direction = 2
+    coordinates = np.copy(polymer.position)
+    for secondMonomers in range(0, len(polymer.afterMiddle)):
+        direction = (direction + polymer.afterMiddle[secondMonomers]-2)%4
+        coordinates += polymer.map[direction]
+        monomerNumber = len(polymer.beforeMiddle)+1+secondMonomers
+        cordTuple = tuple(coordinates)
+        if cordTuple in neighbourDictionary:
+            for nodes in neighbourDictionary[cordTuple]:
+                total += V[nodes, monomerNumber]
+        for neighbour in polymer.map.values():
+            if tuple(coordinates + neighbour) in neighbourDictionary:
+                neighbourDictionary[tuple(coordinates + neighbour)].append(monomerNumber)
+            else:
+                neighbourDictionary[tuple(coordinates + neighbour)] = [monomerNumber]
+
+    return total
+
+def makeDiagonalForceArray(N, background_value):
+    V = np.zeros((N+1,N+1))+background_value
+    for i in range(N):
+        V[i,i] = 0
+        if i > 0:
+            V[i,i-1] = 0
+            V[i-1,i] = 0
+    print(V)
+    return V
+
+N = 15
+V = makeDiagonalForceArray(N, -4*10**(-21))
+for i in range(10):
+    pol, rot = rotateManyTimes(N,1000)
+    print(calculateEnergy(pol, V))
+    illustrationPolymer(pol)
